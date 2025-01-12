a9405ca101100658d14957a36349bcae90a0969a
Boopathi <boopathibm19@gmail.com>
2025-01-12 18:45:57 +0530

    implemented git API

--- src/main/java/com/demo/example/controller/InventoryController.java
+++ src/main/java/com/demo/example/controller/InventoryController.java
@@ -1,7 +1,21 @@
 package com.demo.example.controller;
 
+import com.demo.example.model.Fruit;
+import com.demo.example.service.InventoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("fruit/name")
+    public Fruit getFruitByName(@RequestParam String name){
+        return inventoryService.getFruitByName(name);
+    }
 }
--- src/main/java/com/demo/example/service/InventoryService.java
+++ src/main/java/com/demo/example/service/InventoryService.java
@@ -1,4 +1,20 @@
 package com.demo.example.service;
 
+
+import com.demo.example.model.Fruit;
+import com.demo.example.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InventoryService {
+    private  final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public Fruit getFruitByName(String name) {
+        return inventoryRepository.getFruitByName(name);
+
+    }
 }
